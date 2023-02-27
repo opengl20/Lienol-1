c97e571247b7adceed9419ba5a34402932dd2a86
opengl20 <62195015+opengl20@users.noreply.github.com>
2023-02-28 07:57:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,5 +20,5 @@
 #echo 'src-git shidahuilang https://github.com/shidahuilang/openwrt-package' >>feeds.conf.default
 #echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
